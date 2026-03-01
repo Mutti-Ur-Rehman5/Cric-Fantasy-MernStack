750ccbefe3092850b71a761c1fde9da68d70c745
Mutti-Ur-Rehman5 <muttiu608@gmail.com>
2026-03-01 05:03:26 -0800

    Backend basics+Authentication is set

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// ✅ Connect Database
+connectDB();
+
+// ✅ Middlewares
 app.use(cors());
 app.use(express.json());
 
+
+app.use("/api/auth", require("./routes/auth"));
+
+// ✅ Test Route
 app.get("/", (req, res) => {
   res.send("Fantasy Cricket API Running 🚀");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
